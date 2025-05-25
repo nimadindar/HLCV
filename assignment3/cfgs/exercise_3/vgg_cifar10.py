80e7dd4f371953085d3918ac1e47721a4235c444
nimadindar <nima.dindarsafa@gmail.com>
2025-05-25 19:19:23 +0200

    finished q4

--- assignment3/cfgs/exercise_3/vgg_cifar10.py
+++ assignment3/cfgs/exercise_3/vgg_cifar10.py
@@ -69,3 +69,129 @@ q4_dict = dict(
         wandb = False
     ),
 )
+
+
+q4b_dict = dict(
+    name="CIFAR10_VGG_W_FINETUNE",
+
+    model_arch = VGG11_bn,
+    model_args = dict(
+        num_classes = 10,
+
+        fine_tune=True,
+        weights="IMAGENET1K_V1",
+
+        # The following configs are not for the backbone (that's always a VGG). Rather,
+        # they are the configuration for the MLP head that's applied afterwards!
+        layer_config = [512, 256], 
+        activation = nn.ReLU,
+        norm_layer = nn.BatchNorm1d, # Not 2D! Because these are gonna be 1D features.
+
+    ),
+
+    datamodule = CIFAR10DataModule,
+    data_args=dict(
+        data_dir="data/exercise-2", # You may need to change this for Colab.
+        transform_preset="CIFAR10_VGG",
+        batch_size=64,
+        shuffle=True,
+        heldout_split=0.1,
+        num_workers=6,
+        training=True,
+    ),
+    
+    optimizer=partial(
+        torch.optim.Adam,
+            lr=1e-3, weight_decay=1e-5,
+    ),
+    lr_scheduler=partial(
+        torch.optim.lr_scheduler.ExponentialLR,
+        gamma=0.99, last_epoch=-1,
+    ),
+
+    criterion = nn.CrossEntropyLoss,
+    criterion_args = dict(),
+
+    metrics=dict(
+        top1 = TopKAccuracy(k=1),
+        top5 = TopKAccuracy(k=5),
+    ),
+
+    trainer_module = VGGTrainer,
+    trainer_config = dict(
+        n_gpu = 1,
+        epochs = 15,
+        eval_period = 1,
+        save_dir = "Saved",
+        save_period = 10,
+        monitor = "max eval_top1",
+        early_stop = 5,
+
+        log_step = 100,
+        tensorboard = False,
+        wandb = False
+    ),
+)
+
+
+q4_dict_baseline = dict(
+    name="CIFAR10_VGG_BASELINE",
+
+    model_arch = VGG11_bn,
+    model_args = dict(
+        num_classes = 10,
+
+        fine_tune=True,
+        weights=None,
+
+        # The following configs are not for the backbone (that's always a VGG). Rather,
+        # they are the configuration for the MLP head that's applied afterwards!
+        layer_config = [512, 256], 
+        activation = nn.ReLU,
+        norm_layer = nn.BatchNorm1d, # Not 2D! Because these are gonna be 1D features.
+
+    ),
+
+    datamodule = CIFAR10DataModule,
+    data_args=dict(
+        data_dir="data/exercise-2", # You may need to change this for Colab.
+        transform_preset="CIFAR10_VGG",
+        batch_size=64,
+        shuffle=True,
+        heldout_split=0.1,
+        num_workers=6,
+        training=True,
+    ),
+    
+    optimizer=partial(
+        torch.optim.Adam,
+            lr=1e-3, weight_decay=1e-5,
+    ),
+    lr_scheduler=partial(
+        torch.optim.lr_scheduler.ExponentialLR,
+        gamma=0.99, last_epoch=-1,
+    ),
+
+    criterion = nn.CrossEntropyLoss,
+    criterion_args = dict(),
+
+    metrics=dict(
+        top1 = TopKAccuracy(k=1),
+        top5 = TopKAccuracy(k=5),
+    ),
+
+    trainer_module = VGGTrainer,
+    trainer_config = dict(
+        n_gpu = 1,
+        epochs = 15,
+        eval_period = 1,
+        save_dir = "Saved",
+        save_period = 10,
+        monitor = "max eval_top1",
+        early_stop = 5,
+
+        log_step = 100,
+        tensorboard = False,
+        wandb = False
+    ),
+)
