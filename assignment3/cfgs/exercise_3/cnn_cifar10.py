1bad8bcc5ff860ff55769d265d10d6ae4776d582
nimadindar <nima.dindarsafa@gmail.com>
2025-05-25 17:49:44 +0200

    finished question 3

--- assignment3/cfgs/exercise_3/cnn_cifar10.py
+++ assignment3/cfgs/exercise_3/cnn_cifar10.py
@@ -187,12 +187,519 @@ q2c_earlystop_experiment = dict(
 
 )
 
-q3a_aug1_experiment = ()
-# q3a_aug2_experiment = ()
-# q3a_aug3_experiment = ()
-# ...
+q3a_aug_flip = dict(
+    name = 'CIFAR10_CNN_aug_flip',
+
+    model_arch = ConvNet,
+    model_args = dict(
+        input_size = 3,
+        num_classes = 10,
+        hidden_layers = [128, 512, 512, 512, 512, 512],
+        activation = nn.ReLU(),
+        norm_layer = nn.BatchNorm2d,
+        drop_prob = 0.4,
+    ),
+
+    datamodule = CIFAR10DataModule,
+    data_args = dict(
+        data_dir = "data/exercise-2", # You may need to change this for Colab.
+        transform_preset = 'CIFAR10_WithFlip',
+        batch_size = 200,
+        shuffle = True,
+        heldout_split = 0.1,
+        num_workers = 6,
+    ),
+
+    optimizer = partial(
+        torch.optim.Adam,
+        lr=0.002, weight_decay=0.001, amsgrad=True,
+    ),
+    lr_scheduler = partial(
+        torch.optim.lr_scheduler.StepLR,
+        step_size=5, gamma=0.8
+    ),
+
+    criterion = nn.CrossEntropyLoss,
+    criterion_args = dict(),
+
+    metrics=dict(
+        top1 = TopKAccuracy(k=1),
+        top5 = TopKAccuracy(k=5),
+    ),
+
+    trainer_module = CNNTrainer,
+    trainer_config = dict(
+        n_gpu = 1,
+        epochs = 25,
+        eval_period = 1,
+        save_dir = "Saved",
+        save_period = 10,
+        monitor = "off",
+        early_stop = 0,
+
+        log_step = 100,
+        tensorboard=True,
+        wandb=False,
+    ),
+
+)
 
 
-q3b_dropout_experiment = ()
+q3a_aug_crop_flip = dict(
+    name = 'CIFAR10_CNN_aug_crop_flip',
+
+    model_arch = ConvNet,
+    model_args = dict(
+        input_size = 3,
+        num_classes = 10,
+        hidden_layers = [128, 512, 512, 512, 512, 512],
+        activation = nn.ReLU(),
+        norm_layer = nn.BatchNorm2d,
+        drop_prob = 0.4,
+    ),
+
+    datamodule = CIFAR10DataModule,
+    data_args = dict(
+        data_dir = "data/exercise-2", # You may need to change this for Colab.
+        transform_preset = 'CIFAR10_WithCropFlip',
+        batch_size = 200,
+        shuffle = True,
+        heldout_split = 0.1,
+        num_workers = 6,
+    ),
+
+    optimizer = partial(
+        torch.optim.Adam,
+        lr=0.002, weight_decay=0.001, amsgrad=True,
+    ),
+    lr_scheduler = partial(
+        torch.optim.lr_scheduler.StepLR,
+        step_size=5, gamma=0.8
+    ),
+
+    criterion = nn.CrossEntropyLoss,
+    criterion_args = dict(),
+
+    metrics=dict(
+        top1 = TopKAccuracy(k=1),
+        top5 = TopKAccuracy(k=5),
+    ),
+
+    trainer_module = CNNTrainer,
+    trainer_config = dict(
+        n_gpu = 1,
+        epochs = 25,
+        eval_period = 1,
+        save_dir = "Saved",
+        save_period = 10,
+        monitor = "off",
+        early_stop = 0,
+
+        log_step = 100,
+        tensorboard=True,
+        wandb=False,
+    ),
+
+)
+
+q3a_aug_crop_flip_rotate = dict(
+    name = 'CIFAR10_CNN_aug_crop_flip_rotate',
+
+    model_arch = ConvNet,
+    model_args = dict(
+        input_size = 3,
+        num_classes = 10,
+        hidden_layers = [128, 512, 512, 512, 512, 512],
+        activation = nn.ReLU(),
+        norm_layer = nn.BatchNorm2d,
+        drop_prob = 0.4,
+    ),
+
+    datamodule = CIFAR10DataModule,
+    data_args = dict(
+        data_dir = "data/exercise-2", # You may need to change this for Colab.
+        transform_preset = 'CIFAR10_WithCropFlipRotate',
+        batch_size = 200,
+        shuffle = True,
+        heldout_split = 0.1,
+        num_workers = 6,
+    ),
+
+    optimizer = partial(
+        torch.optim.Adam,
+        lr=0.002, weight_decay=0.001, amsgrad=True,
+    ),
+    lr_scheduler = partial(
+        torch.optim.lr_scheduler.StepLR,
+        step_size=5, gamma=0.8
+    ),
+
+    criterion = nn.CrossEntropyLoss,
+    criterion_args = dict(),
+
+    metrics=dict(
+        top1 = TopKAccuracy(k=1),
+        top5 = TopKAccuracy(k=5),
+    ),
+
+    trainer_module = CNNTrainer,
+    trainer_config = dict(
+        n_gpu = 1,
+        epochs = 25,
+        eval_period = 1,
+        save_dir = "Saved",
+        save_period = 10,
+        monitor = "off",
+        early_stop = 0,
+
+        log_step = 100,
+        tensorboard=True,
+        wandb=False,
+    ),
+
+)
+
+q3a_aug_jitter = dict(
+    name = 'CIFAR10_CNN_aug_jitter',
+
+    model_arch = ConvNet,
+    model_args = dict(
+        input_size = 3,
+        num_classes = 10,
+        hidden_layers = [128, 512, 512, 512, 512, 512],
+        activation = nn.ReLU(),
+        norm_layer = nn.BatchNorm2d,
+        drop_prob = 0.4,
+    ),
+
+    datamodule = CIFAR10DataModule,
+    data_args = dict(
+        data_dir = "data/exercise-2", # You may need to change this for Colab.
+        transform_preset = 'CIFAR10_WithColorJitter',
+        batch_size = 200,
+        shuffle = True,
+        heldout_split = 0.1,
+        num_workers = 6,
+    ),
+
+    optimizer = partial(
+        torch.optim.Adam,
+        lr=0.002, weight_decay=0.001, amsgrad=True,
+    ),
+    lr_scheduler = partial(
+        torch.optim.lr_scheduler.StepLR,
+        step_size=5, gamma=0.8
+    ),
+
+    criterion = nn.CrossEntropyLoss,
+    criterion_args = dict(),
+
+    metrics=dict(
+        top1 = TopKAccuracy(k=1),
+        top5 = TopKAccuracy(k=5),
+    ),
+
+    trainer_module = CNNTrainer,
+    trainer_config = dict(
+        n_gpu = 1,
+        epochs = 25,
+        eval_period = 1,
+        save_dir = "Saved",
+        save_period = 10,
+        monitor = "off",
+        early_stop = 0,
+
+        log_step = 100,
+        tensorboard=True,
+        wandb=False,
+    ),
+
+)
+
+q3a_aug_aggressive = dict(
+    name = 'CIFAR10_CNN_aug_aggressive',
+
+    model_arch = ConvNet,
+    model_args = dict(
+        input_size = 3,
+        num_classes = 10,
+        hidden_layers = [128, 512, 512, 512, 512, 512],
+        activation = nn.ReLU(),
+        norm_layer = nn.BatchNorm2d,
+        drop_prob = 0.4,
+    ),
+
+    datamodule = CIFAR10DataModule,
+    data_args = dict(
+        data_dir = "data/exercise-2", # You may need to change this for Colab.
+        transform_preset = 'CIFAR10_Aggressive',
+        batch_size = 200,
+        shuffle = True,
+        heldout_split = 0.1,
+        num_workers = 6,
+    ),
+
+    optimizer = partial(
+        torch.optim.Adam,
+        lr=0.002, weight_decay=0.001, amsgrad=True,
+    ),
+    lr_scheduler = partial(
+        torch.optim.lr_scheduler.StepLR,
+        step_size=5, gamma=0.8
+    ),
+
+    criterion = nn.CrossEntropyLoss,
+    criterion_args = dict(),
+
+    metrics=dict(
+        top1 = TopKAccuracy(k=1),
+        top5 = TopKAccuracy(k=5),
+    ),
+
+    trainer_module = CNNTrainer,
+    trainer_config = dict(
+        n_gpu = 1,
+        epochs = 25,
+        eval_period = 1,
+        save_dir = "Saved",
+        save_period = 10,
+        monitor = "off",
+        early_stop = 0,
+
+        log_step = 100,
+        tensorboard=True,
+        wandb=False,
+    ),
+
+)
+
+
+q3b_dropout0 = dict(
+    name = 'CIFAR10_drop_0',
+
+    model_arch = ConvNet,
+    model_args = dict(
+        input_size = 3,
+        num_classes = 10,
+        hidden_layers = [128, 512, 512, 512, 512, 512],
+        activation = nn.ReLU(),
+        norm_layer = nn.BatchNorm2d,
+        drop_prob = 0,
+    ),
+
+    datamodule = CIFAR10DataModule,
+    data_args = dict(
+        data_dir = "data/exercise-2", # You may need to change this for Colab.
+        transform_preset = 'CIFAR10',
+        batch_size = 200,
+        shuffle = True,
+        heldout_split = 0.1,
+        num_workers = 6,
+    ),
+
+    optimizer = partial(
+        torch.optim.Adam,
+        lr=0.002, weight_decay=0.001, amsgrad=True,
+    ),
+    lr_scheduler = partial(
+        torch.optim.lr_scheduler.StepLR,
+        step_size=5, gamma=0.8
+    ),
+
+    criterion = nn.CrossEntropyLoss,
+    criterion_args = dict(),
+
+    metrics=dict(
+        top1 = TopKAccuracy(k=1),
+        top5 = TopKAccuracy(k=5),
+    ),
+
+    trainer_module = CNNTrainer,
+    trainer_config = dict(
+        n_gpu = 1,
+        epochs = 10,
+        eval_period = 1,
+        save_dir = "Saved",
+        save_period = 10,
+        monitor = "off",
+        early_stop = 0,
+
+        log_step = 100,
+        tensorboard=True,
+        wandb=False,
+    ),
+
+)
+
+q3b_dropout2 = dict(
+    name = 'CIFAR10_drop_2',
+
+    model_arch = ConvNet,
+    model_args = dict(
+        input_size = 3,
+        num_classes = 10,
+        hidden_layers = [128, 512, 512, 512, 512, 512],
+        activation = nn.ReLU(),
+        norm_layer = nn.BatchNorm2d,
+        drop_prob = 0.2,
+    ),
+
+    datamodule = CIFAR10DataModule,
+    data_args = dict(
+        data_dir = "data/exercise-2", # You may need to change this for Colab.
+        transform_preset = 'CIFAR10',
+        batch_size = 200,
+        shuffle = True,
+        heldout_split = 0.1,
+        num_workers = 6,
+    ),
+
+    optimizer = partial(
+        torch.optim.Adam,
+        lr=0.002, weight_decay=0.001, amsgrad=True,
+    ),
+    lr_scheduler = partial(
+        torch.optim.lr_scheduler.StepLR,
+        step_size=5, gamma=0.8
+    ),
+
+    criterion = nn.CrossEntropyLoss,
+    criterion_args = dict(),
+
+    metrics=dict(
+        top1 = TopKAccuracy(k=1),
+        top5 = TopKAccuracy(k=5),
+    ),
+
+    trainer_module = CNNTrainer,
+    trainer_config = dict(
+        n_gpu = 1,
+        epochs = 10,
+        eval_period = 1,
+        save_dir = "Saved",
+        save_period = 10,
+        monitor = "off",
+        early_stop = 0,
+
+        log_step = 100,
+        tensorboard=True,
+        wandb=False,
+    ),
+
+)
+
+q3b_dropout6 = dict(
+    name = 'CIFAR10_drop_6',
+
+    model_arch = ConvNet,
+    model_args = dict(
+        input_size = 3,
+        num_classes = 10,
+        hidden_layers = [128, 512, 512, 512, 512, 512],
+        activation = nn.ReLU(),
+        norm_layer = nn.BatchNorm2d,
+        drop_prob = 0.6,
+    ),
+
+    datamodule = CIFAR10DataModule,
+    data_args = dict(
+        data_dir = "data/exercise-2", # You may need to change this for Colab.
+        transform_preset = 'CIFAR10',
+        batch_size = 200,
+        shuffle = True,
+        heldout_split = 0.1,
+        num_workers = 6,
+    ),
+
+    optimizer = partial(
+        torch.optim.Adam,
+        lr=0.002, weight_decay=0.001, amsgrad=True,
+    ),
+    lr_scheduler = partial(
+        torch.optim.lr_scheduler.StepLR,
+        step_size=5, gamma=0.8
+    ),
+
+    criterion = nn.CrossEntropyLoss,
+    criterion_args = dict(),
+
+    metrics=dict(
+        top1 = TopKAccuracy(k=1),
+        top5 = TopKAccuracy(k=5),
+    ),
+
+    trainer_module = CNNTrainer,
+    trainer_config = dict(
+        n_gpu = 1,
+        epochs = 10,
+        eval_period = 1,
+        save_dir = "Saved",
+        save_period = 10,
+        monitor = "off",
+        early_stop = 0,
+
+        log_step = 100,
+        tensorboard=True,
+        wandb=False,
+    ),
+
+)
+
+q3b_dropout9 = dict(
+    name = 'CIFAR10_drop_9',
+
+    model_arch = ConvNet,
+    model_args = dict(
+        input_size = 3,
+        num_classes = 10,
+        hidden_layers = [128, 512, 512, 512, 512, 512],
+        activation = nn.ReLU(),
+        norm_layer = nn.BatchNorm2d,
+        drop_prob = 0.9,
+    ),
+
+    datamodule = CIFAR10DataModule,
+    data_args = dict(
+        data_dir = "data/exercise-2", # You may need to change this for Colab.
+        transform_preset = 'CIFAR10',
+        batch_size = 200,
+        shuffle = True,
+        heldout_split = 0.1,
+        num_workers = 6,
+    ),
+
+    optimizer = partial(
+        torch.optim.Adam,
+        lr=0.002, weight_decay=0.001, amsgrad=True,
+    ),
+    lr_scheduler = partial(
+        torch.optim.lr_scheduler.StepLR,
+        step_size=5, gamma=0.8
+    ),
+
+    criterion = nn.CrossEntropyLoss,
+    criterion_args = dict(),
+
+    metrics=dict(
+        top1 = TopKAccuracy(k=1),
+        top5 = TopKAccuracy(k=5),
+    ),
+
+    trainer_module = CNNTrainer,
+    trainer_config = dict(
+        n_gpu = 1,
+        epochs = 10,
+        eval_period = 1,
+        save_dir = "Saved",
+        save_period = 10,
+        monitor = "off",
+        early_stop = 0,
+
+        log_step = 100,
+        tensorboard=True,
+        wandb=False,
+    ),
+
+)
 
 # define more config dictionaries if needed...
--- assignment3/src/utils/transform_presets.py
+++ assignment3/src/utils/transform_presets.py
@@ -19,7 +19,77 @@ presets = dict(
     # Note that you usually need the augmentation **only** for training time!
     # E.g. CIFAR10_WithFlip=dict()
     ##############
-    CIFAR10_WithFlip=dict(
+
+    # Augmentation with Horizontal Flipping
+    CIFAR10_WithFlip = dict(
+        train= transforms.Compose([
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]),
+        eval=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    ),
+
+    # Augmentation with Random cropping with padding added to flipping
+    CIFAR10_WithCropFlip = dict(
+        train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]),
+        eval=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    ),
+
+    # Augmentation with rotation, cropping and flipping
+    CIFAR10_WithCropFlipRotate=dict(
+        train=transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomRotation(10),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]),
+        eval=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    ),
+
+    # Augmentation with flipping and colorjitter
+    CIFAR10_WithColorJitter=dict(
+        train=transforms.Compose([
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]),
+        eval=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    ),
+
+    # Combines all augmentations for maximum diversity
+    CIFAR10_Aggressive=dict(
+        train=transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomRotation(10),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]),
+        eval=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
     ),
 
     #  This one is for Question 4.
