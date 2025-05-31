80da2947aaf2c2de26556ef857f649559cee07e8
nimadindar <nima.dindarsafa@gmail.com>
2025-05-31 13:52:40 +0200

    Finished report for assignment 3

--- assignment3/cfgs/exercise_3/cnn_cifar10.py
+++ assignment3/cfgs/exercise_3/cnn_cifar10.py
@@ -130,8 +130,8 @@ q2a_normalization_experiment = dict(
 
 )
 
-q2c_earlystop_experiment = dict(
-    name = 'CIFAR10_CNN_EARLY_STOP',
+q2b_experiment = dict(
+    name = 'CIFAR10_CNN_EARLY_STOP1',
 
     model_arch = ConvNet,
     model_args = dict(
@@ -173,7 +173,64 @@ q2c_earlystop_experiment = dict(
     trainer_module = CNNTrainer,
     trainer_config = dict(
         n_gpu = 1,
-        epochs = 10,
+        epochs = 50,
+        eval_period = 1,
+        save_dir = "Saved",
+        save_period = 10,
+        monitor = "max eval_top1",
+        early_stop = 4,
+
+        log_step = 100,
+        tensorboard=True,
+        wandb=False,
+    ),
+
+)
+
+q2c_earlystop_experiment = dict(
+    name = 'CIFAR10_CNN_EARLY_STOP2',
+
+    model_arch = ConvNet,
+    model_args = dict(
+        input_size = 3,
+        num_classes = 10,
+        hidden_layers = [128, 512, 512, 512, 512, 512],
+        activation = nn.ReLU(),
+        norm_layer = nn.BatchNorm2d,
+        drop_prob = 0.4,
+    ),
+
+    datamodule = CIFAR10DataModule,
+    data_args = dict(
+        data_dir = "data/exercise-2", # You may need to change this for Colab.
+        transform_preset = 'CIFAR10',
+        batch_size = 200,
+        shuffle = True,
+        heldout_split = 0.1,
+        num_workers = 6,
+    ),
+
+    optimizer = partial(
+        torch.optim.Adam,
+        lr=0.002, weight_decay=0.001, amsgrad=True,
+    ),
+    lr_scheduler = partial(
+        torch.optim.lr_scheduler.StepLR,
+        step_size=5, gamma=0.8
+    ),
+
+    criterion = nn.CrossEntropyLoss,
+    criterion_args = dict(),
+
+    metrics=dict(
+        top1 = TopKAccuracy(k=1),
+        top5 = TopKAccuracy(k=5),
+    ),
+
+    trainer_module = CNNTrainer,
+    trainer_config = dict(
+        n_gpu = 1,
+        epochs = 50,
         eval_period = 1,
         save_dir = "Saved",
         save_period = 10,
